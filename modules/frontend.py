f678b0eae439dea02bd0589a6fff15414589e816
tr1x_em <admin@trix.is-a.dev>
2024-12-11 16:14:58 +0530

    greatest  commit ever

--- modules/frontend.py
+++ modules/frontend.py
@@ -8,7 +8,7 @@ import time
 from nicegui import ui,app
 import json
 import re
-
+from modules.sqlwrapper import  *
 
 def init(fastapi_app: FastAPI):
 
@@ -62,20 +62,15 @@ def register():
     selected_option = option_input.value
 
     if username and password and name:
-        if username in users:
-            ui.notify("Username already exists.", color='negative')
-        else:
-            users[username] = {
-                'password': password,
-                'name': name,
-                'option': selected_option
-            }
-            save_users(users)
+        if checkuser(username):
+            registeruser(username, password, name, selected_option)
             ui.notify("Registration successful!", color='positive')
             username_input.set_value('')
             password_input.set_value('')
             name_input.set_value('')
             ui.navigate.to('/login')
+        else:
+            ui.notify("Username already exists.", color='negative')
     else:
         ui.notify("All fields are required.", color='negative')
 
@@ -163,19 +158,22 @@ def me_page() -> None:
         ui.navigate.to('/login')
         ui.notify('You have been logged out.')
     username = app.storage.user["username"]
-    user_info = users.get(username, {})
+    if  not checkuser(username):
+        user_info = getuser(username)
+        name = user_info[2]
+        avatar = get_avatar(name,str(user_info[3]))
+        print(avatar)
+        with ui.column().classes('absolute-center items-center'):
+            ui.image(avatar).classes('rounded-full w-48 h-48 ml-4')
+            ui.label(f'Hello {name}!').classes('text-h3')
+            ui.label(f'Username:{username}').classes('text-lg')
+            ui.label(f'Avatar: {user_info[3]}').classes('text-lg')
+            with ui.row().classes('items-center'):
+                ui.button("Logout", on_click=logout, icon='logout')
+            ui.button("chat", on_click=lambda: ui.navigate.to('/chat'), icon='chat')
+    else:
+        logout()
 
-    name = user_info.get('name', 'Unknown User')
-    avatar = get_avatar(name,user_info.get('option'))
-
-    with ui.column().classes('absolute-center items-center'):
-        ui.image(avatar).classes('rounded-full w-48 h-48 ml-4')
-        ui.label(f'Hello {user_info.get("name", "Unknown User")}!').classes('text-h3')
-        ui.label(f'Username:{username}').classes('text-lg')
-        ui.label(f'Avatar: {user_info.get("option", "No Option Selected")}').classes('text-lg')
-        with ui.row().classes('items-center'):
-           ui.button("Logout", on_click=logout, icon='logout')
-           ui.button("chat", on_click=lambda: ui.navigate.to('/chat'), icon='chat')
 
 @ui.page('/')
 def main_page() -> None:
@@ -221,7 +219,7 @@ def login() -> Optional[RedirectResponse]:
         username_value = username.value.strip()
         password_value = password.value.strip()
 
-        if username_value in users and users[username_value]['password'] == password_value:
+        if loginuser(username_value, password_value):
             app.storage.user.update({'username': username_value, 'authenticated': True})
 
             ui.navigate.to(app.storage.user.get('referrer_path', '/me'))
@@ -321,7 +319,6 @@ def chat_messages(own_id: str) -> None:
     ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)')
 
 
-
 replying_to = None
 def send_reply(msg_text, sender_name):
     global replying_to
@@ -352,11 +349,11 @@ async def main():
                     formatted_message = f"<div style='color: gray; font-style: italic;'>Replying to {replying_to[0]}:<br>{replying_to[1][:max_length] }</div><div>{reply_text}</div>"
                     print(f"Reply 2 : {formatted_message}")
 
-                messages.append((user_id, avatar, formatted_message, stamp))
+                messages.append((username, avatar, formatted_message, stamp))
                 replying_to=None
             else:
 
-                messages.append((user_id, avatar, f"{text.value}", stamp))
+                messages.append((username, avatar, f"{text.value}", stamp))
 
             text.value = ''
             chat_messages.refresh()
@@ -367,51 +364,53 @@ async def main():
             ui.button(icon='dark_mode', on_click=lambda: dark_mode.set_value(None)).props('flat fab-mini color=white').bind_visibility_from(dark_mode, 'value', value=True)
             ui.button(icon='light_mode', on_click=lambda: dark_mode.set_value(True)).props('flat fab-mini color=black').bind_visibility_from(dark_mode, 'value', value=False)
             ui.button(icon='brightness_auto', on_click=lambda: dark_mode.set_value(False)).props('flat fab-mini color=white').bind_visibility_from(dark_mode, 'value', lambda mode: mode is None)
+
     username = app.storage.user["username"]
-    user_info = users.get(username, {})
-    name = user_info.get('name', 'Unknown User')
+    if  not checkuser(username):
+        user_info = getuser(username)
+        name = user_info[2]
+        avatar = get_avatar(name,user_info[3])
 
-    user_id = app.storage.user["username"]
-    avatar = get_avatar(name,user_info.get('option'))
-    clients.append(ui.context.client)
+        clients.append(ui.context.client)
 
-    with ui.footer().classes('bg-black'):
-        with ui.column().classes('w-full max-w-3xl mx-auto my-6'):
-            with ui.row().classes('w-full no-wrap items-center'):
-                with ui.avatar().on('click', lambda: ui.navigate.to(main)):
-                    ui.image(avatar)
+        with ui.footer().classes('bg-black'):
+            with ui.column().classes('w-full max-w-3xl mx-auto my-6'):
+                with ui.row().classes('w-full no-wrap items-center'):
+                    with ui.avatar().on('click', lambda: ui.navigate.to(main)):
+                        ui.image(avatar)
 
-                text = ui.input(placeholder='message').on('keydown.enter', send).props('rounded outlined input-class=mx-3').classes('flex-grow')
+                    text = ui.input(placeholder='message').on('keydown.enter', send).props('rounded outlined input-class=mx-3').classes('flex-grow')
 
-                ui.button(icon="send",on_click=send).classes('ml-3 flex items-center').props('rounded')
+                    ui.button(icon="send",on_click=send).classes('ml-3 flex items-center').props('rounded')
 
-    ui.add_css(r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500; } {background-image: url("https://i.imgur.com/Gk3qlQ2.png"); background-size: cover; background-position: center; background-repeat: no-repeat;}')
-    online_users[str(ui.context.client)] = {
+
+        ui.add_css(r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500; } {background-image: url("https://i.imgur.com/Gk3qlQ2.png"); background-size: cover; background-position: center; background-repeat: no-repeat;}')
+        online_users[str(ui.context.client)] = {
             "name": name,
             "status": "online",
             "avatar": avatar
         }
-    ui.run_javascript(f"""
-        window.addEventListener('beforeunload', function(event) {{
-            const userId = '{ui.context.client}'; // Dynamic user ID
+        ui.run_javascript(f"""
+            window.addEventListener('beforeunload', function(event) {{
+                const userId = '{ui.context.client}'; // Dynamic user ID
 
-            fetch('/api/user/disconnect', {{
-                method: 'POST',
-                headers: {{
-                    'Content-Type': 'application/json'
-                }},
-                body: JSON.stringify({{ userId: userId }})
+                fetch('/api/user/disconnect', {{
+                    method: 'POST',
+                    headers: {{
+                        'Content-Type': 'application/json'
+                    }},
+                    body: JSON.stringify({{ userId: userId }})
+                }});
             }});
-        }});
-    """)
-    broadcast_message(f"{name} is now connected.",ui.context.client)
-    with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
-        with ui.column().style("position: absolute; top: 0; left: 0; height: 80%; width: 30%; z-index: 1; padding: 10px;"):
-            update_user()
+        """)
+        broadcast_message(f"{name} is now connected.",ui.context.client)
+        with ui.column().classes('w-full max-w-2xl mx-auto items-stretch'):
+            with ui.column().style("position: absolute; top: 0; left: 0; height: 80%; width: 30%; z-index: 1; padding: 10px;"):
+                update_user()
 
-        # Ensuring chat messages appear in the remaining space
-        with ui.column().style("margin-left: 30%;"):
-            chat_messages(user_id)
-
-
+            # Ensuring chat messages appear in the remaining space
+            with ui.column().style("margin-left: 5%;"):
+                chat_messages(username)
+    else:
+        ui.navigate.to('/login')
 
